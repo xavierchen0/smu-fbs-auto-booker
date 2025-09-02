39ce2fa975c527a91af4b710452b5e2ba1f43c05
Xavier Chen <xaviernehc@gmail.com>
2025-09-02 17:07:08 +0800

    feat: remove --env-file flag for github actions

--- run_autobooking.sh
+++ run_autobooking.sh
@@ -2,12 +2,12 @@
 
 # Run main script
 echo "Running main.js"
-node --env-file=.env main.js
+node main.js
 echo "main.js Finished Successfully"
 
 # Only update booking date if main.js succeeded
 echo "Updating Booking Date"
 if [ $? -eq 0 ]; then
-    node --env-file=.env updateBookingDate.js
+    node updateBookingDate.js
 fi
 echo "Booking Date Updated"
