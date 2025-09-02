52ec1b40edbd7e723c3326236a848e8fb11e0525
Xavier Chen <xaviernehc@gmail.com>
2025-09-02 17:12:20 +0800

    feat: add back --env-file flag
    github action will not run run_autobooking.sh

--- run_autobooking.sh
+++ run_autobooking.sh
@@ -2,12 +2,12 @@
 
 # Run main script
 echo "Running main.js"
-node main.js
+node env-file=.env main.js
 echo "main.js Finished Successfully"
 
 # Only update booking date if main.js succeeded
 echo "Updating Booking Date"
 if [ $? -eq 0 ]; then
-    node updateBookingDate.js
+    node --env-file=.env updateBookingDate.js
 fi
 echo "Booking Date Updated"
